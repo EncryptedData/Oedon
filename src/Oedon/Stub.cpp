98a9128b9c0434de17c96132e47228f0bf8c1578
Russell Johnson <russell.johnson@superdark.net>
2022-04-15 20:50:17 -0700

    Fix for Bryan's vcpkg problems

--- src/Oedon/Stub.cpp
+++ src/Oedon/Stub.cpp
@@ -1,8 +1,16 @@
 
 #include "common.h"
-#include "glm/glm.hpp"
+#include <glm/glm.hpp>
+#include <assimp/scene.h>
 
+// External C functions have to be marked like this
 OEDONAPI int Stub(int a, int b)
 {
 	return a + b;
 }
+
+// External classes need to be marked like this
+class OEDONCLASS MeshStub
+{
+
+};
--- src/Oedon/common.h
+++ src/Oedon/common.h
@@ -3,11 +3,12 @@
 #ifdef _WIN32
 
 #define OEDONAPI extern "C" __declspec(dllexport)
+#define OEDONCLASS __declspec(dllexport)
 
 #else
 
 #define OEDONAPI extern "C"
-
+#define OEDONCLASS 
 #endif
 
 
